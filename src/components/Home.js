d64154a33c278a866ecf588c51df012fe2990f54
student <kmac.kimaru93@gmail.com>
2024-04-22 22:20:26 +0300

    adding jsx

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,15 @@ import { name, city } from "../data/data.js";
 
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+const headStyle = {
+color: 'firebrick',
+}
+  return (
+<div id="home">
+<h1 style ={headStyle}>{name} is a Web Developer from {city}</h1>
+</div>
+  )
+  ;
 }
 
 export default Home;
